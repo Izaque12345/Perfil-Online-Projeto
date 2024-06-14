fc58ce4c6bca59cffbf797bf1fb27865eaff1f99
Izaque12345 <andradeizaque01@Gmail.com>
2024-06-14 19:36:42 -0300

    Cabeçalho

---
+++ docs/home/home.js
@@ -0,0 +1,6 @@
+var banner = window.document.getElementById("banner");
+var logoFoto = window.document.getElementById("logo-foto");
+var title = window.document.getElementById("title");
+window.onload = function() {
+  title.innerText = "NUT BR"
+}
