07e19a589c4bcfc0fa80fec0e850531dc2e0a65d
Izaque12345 <andradeizaque01@Gmail.com>
2024-06-15 13:09:18 -0300

    Reorganização dos arquivos

--- docs/home.config/home.js
+++ docs/home.config/home.js
@@ -1,6 +1,7 @@
 var banner = window.document.getElementById("banner");
 var logoFoto = window.document.getElementById("logo-foto");
 var title = window.document.getElementById("title");
+var info = 
 window.onload = function() {
   title.innerText = "NUT BR"
 }
